5dc9f5a14d279e121031ad5864fb0cb76ded8d4e
poojaprasad1270 <poojaprasad1270@gmail.com>
2021-05-04 02:48:37 +0530

    added Firebase support

---
+++ app/src/main/java/uk/ac/tees/aad/W9520874/monthlyTrans.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.aad.W9520874;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class monthlyTrans extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monthly_trans);
+    }
+}
